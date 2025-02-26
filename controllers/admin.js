d441f0de5cb34387516d686ca5ff96bcc4eda5e3
vinit2283 <vinitpatel228@gmail.com>
2025-02-26 14:55:17 +0530

    first mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -9,7 +9,7 @@ exports.getAddProduct = (req, res, next) => {
         pageTitle: 'Add Product',
         path: '/admin/add-product',
         editing: false,
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: req.session.isLoggedIn
         // formsCSS: true,
         // productCSS: true,
         // activeAddProduct: true
@@ -50,7 +50,7 @@ exports.getEditProduct = (req, res, next) => {
                 path: '/admin/edit-product',
                 editing: editMode, //This is a flag that we can use in the template to determine if we are in edit mode or not
                 product: product,
-                isAuthenticated: req.isLoggedIn //This is a flag that we can use in the template to determine if the user is authenticated
+                isAuthenticated: req.session.isLoggedIn //This is a flag that we can use in the template to determine if the user is authenticated
             });
         })
         .catch(err => {
@@ -108,7 +108,7 @@ exports.getProducts = (req, res, next) => {
                 prods: products,
                 pageTitle: 'Admin Products',
                 path: '/admin/products',
-                isAuthenticated: req.isLoggedIn
+                isAuthenticated: req.session.isLoggedIn
             });
         })
         .catch(err => {
--- controllers/error.js
+++ controllers/error.js
@@ -1,7 +1,7 @@
 exports.get404Page = (req, res, next) => {
     res.status(404).render('404', {
         pageTitle: 'Error Page', path: '/404',
-        isAuthenticated: req.isLoggedIn
+        isAuthenticated: req.session.isLoggedIn
     });
     // res.status(404).sendFile(path.join(__dirname, 'views','404.html'));
 };
