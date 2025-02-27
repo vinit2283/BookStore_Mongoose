c3b6c398c82d0ca690c893b2f62ab058f3053a9a
vinit2283 <vinitpatel228@gmail.com>
2025-02-27 10:46:39 +0530

    working mongoose

--- controllers/auth.js
+++ controllers/auth.js
@@ -12,9 +12,16 @@ exports.getLogin = (req, res, next) => {
         isAuthenticated: false
         // isAuthenticated: req.isLoggedIn //This is a flag that we can use in the template to determine if the user is authenticated
     });
-}
+};
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+        path: '/signup',
+        pageTitle: 'Signup',
+        isAuthenticated: false
+    });
+};
 exports.postLogin = (req, res, next) => {
-    User.findById('67bc37038c80101b3a2df835')
+    User.findById('67bfec3128d771cc3f7bdee2')
         .then(user => {
             req.session.isLoggedIn = true;
             req.session.user = user;
@@ -28,7 +35,30 @@ exports.postLogin = (req, res, next) => {
         });
     // res.setHeader('set-Cookie', 'loggedIn=true');
     // req.isLoggedIn = true; // storing the information that i am loggedin in this loggedin
-}
+};
+exports.postSignup = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const confirmPassword = req.body.confirmPassword;
+    User.findOne({ email: email })
+        .then(userDoc => {
+            if (userDoc) {
+                return res.redirect('/signup');
+            }
+            const user = new User({
+                email: email,
+                password: password,
+                cart: { items: [] }
+            });
+            return user.save();
+        })
+        .then(result => {
+            res.redirect('/login');
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
 exports.postLogout = (req, res, next) => {
     req.session.destroy(err => {
         console.log(err);
